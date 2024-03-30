6a9d72a4c33e7e539280349aced4a467fe1005fd
pyovski <dev.natalyalves@gmail.com>
2024-03-30 20:54:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using bootcamp_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora();
+
+        calc.Somar(10, 20.4);
+        calc.Dividir(10, 4);
+        calc.Subtrair(10, 5);
+        calc.Dividir(40, 4);
+   }
+}
