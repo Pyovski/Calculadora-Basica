567f7c6f5a9e2eb16a5466f67a9857e2bf97ffeb
pyovski <dev.natalyalves@gmail.com>
2024-03-30 21:39:20 -0300

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
-﻿using bootcamp_2;
+﻿using System;
+using bootcamp_2;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Digite o primeiro número:");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número:");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
         Calculadora calc = new Calculadora();
 
-        calc.Somar(10, 20.4);
-        calc.Dividir(10, 4);
-        calc.Subtrair(10, 5);
-        calc.Dividir(40, 4);
-   }
+        Console.WriteLine("Escolha a operação: ");
+        Console.WriteLine("1 - Somar");
+        Console.WriteLine("2 - Subtrair");
+        Console.WriteLine("3 - Multiplicar");
+        Console.WriteLine("4 - Dividir");
+
+        int opcao = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcao)
+        {
+            case 1:
+                calc.Somar(numero1, numero2);
+                break;
+            case 2:
+                calc.Subtrair(numero1, numero2);
+                break;
+            case 3:
+                calc.Multiplicar(numero1, numero2);
+                break;
+            case 4:
+                calc.Dividir(numero1, numero2);
+                break;
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+    }
 }
